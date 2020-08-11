d08f794d85bff969062aae76ffed582ff8ecc6f8
mrocca1998 <mrocca1998@gmail.com>
2020-08-11 13:05:10 -0400

    Allocation put checks

--- CapacityManagementAPI/Controllers/AllocationsController.cs
+++ CapacityManagementAPI/Controllers/AllocationsController.cs
@@ -53,8 +53,125 @@ namespace CapacityManagementAPI.Controllers
             return allocation;
         }
 
-        // PUT: api/Allocations/5
-        [HttpPut("{id}")]
+        // PUT: api/Allocations/Check/5
+        [HttpPut("Check/{id}")]
+        public async Task<ActionResult<String>> PutAllocationCheck(int id, [FromBody] Allocation allocation)
+        {
+            Dictionary<int, string> months = new Dictionary<int, string> { { 1, "January" }, { 2, "February" }, { 3, "March" }, { 4, "April" }, { 5, "May" }, { 6, "June" }, { 7, "July" }, { 8, "August" }, { 9, "September" }, { 10, "October" }, { 11, "November" }, { 12, "December" } };
+            if (!allocation.EndDate.HasValue)
+            {
+                allocation.EndDate = allocation.StartDate;
+            }
+            if (DateTime.Compare((DateTime)allocation.StartDate, (DateTime)allocation.EndDate) > 0)
+            {
+                string message = "Entry error: Allocation start date is after the end date";
+                return message;
+            }
+
+            var currentAllo2 = 0;
+            var doubleAllo = new Dictionary<DateTime, int>();
+            DateTime date = (DateTime)allocation.StartDate;
+            var allocations = await _context.Allocations.ToListAsync();
+
+
+            //check if the current month is within the allocation's span
+            while (DateTime.Compare(date, (DateTime)allocation.EndDate) <= 0)
+            {
+                currentAllo2 = 0;
+                foreach (Allocation allocation2 in allocations)
+                {
+                    //check if an allocation contains the current month and is assigned to the same employee
+                    if (DateTime.Compare(date, (DateTime)allocation2.StartDate) >= 0
+                            && DateTime.Compare(date, (DateTime)allocation2.EndDate) <= 0
+                            && allocation.EmployeeId.Equals(allocation2.EmployeeId)
+                            && allocation.ProjectId.Equals(allocation2.ProjectId)
+                            && allocation.Role.Equals(allocation2.Role)
+                            && id != allocation2.Id)
+                    {
+                        currentAllo2 = allocation2.Id;
+                    }
+                }
+                if (currentAllo2 > 0)
+                {
+                    doubleAllo[date] = currentAllo2;
+                }
+
+                date = date.AddMonths(1);
+            }
+
+            //check for double allocation
+            if (doubleAllo.Count > 0)
+            {
+                string dates = "";
+                foreach (DateTime date1 in doubleAllo.Keys)
+                {
+                    dates += Environment.NewLine + months[date1.Month] + " " + date1.Year.ToString();
+                }
+                string message = "Error: " + _context.Employees.FirstOrDefault(e => e.Id == allocation.EmployeeId).name + " is already allocated as a " + allocation.Role + " to " + _context.Projects.FirstOrDefault(p => p.Id == allocation.ProjectId).Title + dates;
+                return message;
+
+            }
+
+            date = (DateTime)allocation.StartDate;
+            allocations = await _context.Allocations.ToListAsync();
+            var overAllo = new Dictionary<DateTime, List<int>>();
+            var currentAllo = new List<int>();
+            var alloTotal = allocation.Allocation1;
+
+            //check if the current month is within the allocation's span
+            while (DateTime.Compare(date, (DateTime)allocation.EndDate) <= 0)
+            {
+                currentAllo = new List<int>();
+                //running count of the total allocation for that employee that month
+                alloTotal = allocation.Allocation1;
+                foreach (Allocation allocation2 in allocations)
+                {
+                    //check if an allocation contains the current month and is assigned to the same employee
+                    if (DateTime.Compare(date, (DateTime)allocation2.StartDate) >= 0
+                            && DateTime.Compare(date, (DateTime)allocation2.EndDate) <= 0
+                            && allocation.EmployeeId.Equals(allocation2.EmployeeId)
+                            && id != allocation2.Id)
+                    {
+                        //add that allocation to the current toal of that employee's allocations for the month and save the allocation id
+                        currentAllo.Add(allocation2.Id);
+                        alloTotal += allocation2.Allocation1;
+                    }
+                    //check if the employee is overallocated at the current month
+                    if (alloTotal > 100)
+                    {
+                        //save the month and the list of allocation id's for that month
+                        overAllo[date] = currentAllo;
+                    }
+                }
+                date = date.AddMonths(1);
+            }
+            string overAlloDates = "";
+            double totalAllo = 0;
+            if (overAllo.Count > 0)
+            {
+                foreach (KeyValuePair<DateTime, List<int>> date2 in overAllo)
+                {
+                    totalAllo = (double)allocation.Allocation1;
+                    overAlloDates += Environment.NewLine + Environment.NewLine + months[date2.Key.Month] + " " + date2.Key.Year.ToString();
+                    foreach (int allocationID in date2.Value)
+                    {
+                        Allocation allocation2 = _context.Allocations.FirstOrDefault(a => a.Id == allocationID);
+                        totalAllo += (double)allocation2.Allocation1;
+                        overAlloDates += Environment.NewLine + _context.Projects.FirstOrDefault(p => p.Id == allocation2.ProjectId).Title + " " + allocation2.Allocation1 + "%";
+                    }
+                    overAlloDates += Environment.NewLine + _context.Projects.FirstOrDefault(p => p.Id == allocation.ProjectId).Title + " " + allocation.Allocation1 + "%";
+                    overAlloDates += Environment.NewLine + "Total Allocation: " + totalAllo + "%";
+                }
+                string eMessage = "Entry error: " + _context.Employees.FirstOrDefault(e => e.Id == allocation.EmployeeId).name + " would be overallocated:" + overAlloDates;
+                return eMessage;
+
+            }
+
+            return "";
+        }
+
+            // PUT: api/Allocations/5
+            [HttpPut("{id}")]
         public async Task<ActionResult<String>> PutAllocation(int id, [FromBody]Allocation allocation)
         {
 
